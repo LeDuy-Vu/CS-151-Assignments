df8284e8f2cb8c313d0047f70e98841889c00719
Le Duy Vu <vlduy2112@gmail.com>
2020-05-29 22:58:48 -0700

    Represent a calendar day in the Gregorian calendar

---
+++ Assignment 4/DayTester.java
@@ -0,0 +1,89 @@
+public class DayTester
+{
+	public static void main(String[] args) throws IllegalArgumentException
+	{
+		try
+		{
+			Day dayError = new Day (0, 21, 12) ;
+		}
+		catch (IllegalArgumentException ex)
+		{
+			System.out.println("Should print error: " + ex) ;
+		}
+		
+		try
+		{
+			Day dayError = new Day (2019, 13, 1) ;
+		}
+		catch (IllegalArgumentException ex)
+		{
+			System.out.println("Should print error: " + ex) ;
+		}
+		
+		try
+		{
+			Day dayError = new Day (2019, 10, 33) ;
+		}
+		catch (IllegalArgumentException ex)
+		{
+			System.out.println("Should print error: " + ex) ;
+		}
+		
+		try
+		{
+			Day dayError = new Day (1582, 10, 7) ;
+		}
+		catch (IllegalArgumentException ex)
+		{
+			System.out.println("Should print error: " + ex) ;
+		}
+		
+		try
+		{
+			Day dayError = new Day (2019, 2, 29) ;
+		}
+		catch (IllegalArgumentException ex)
+		{
+			System.out.println("Should print error: " + ex) ;
+		}
+		
+		try
+		{
+			Day dayError = new Day (1500, 2, 30) ;
+		}
+		catch (IllegalArgumentException ex)
+		{
+			System.out.println("Should print error: " + ex) ;
+		}
+		
+		try
+		{
+			Day dayError = new Day (2019, 11, 31) ;
+		}
+		catch (IllegalArgumentException ex)
+		{
+			System.out.println("Should print error: " + ex + "\n") ;
+		}
+		
+		Day day1 = new Day(1582, 10, 15) ;
+		Day day2 = day1.addDays(-3) ;
+		System.out.println("Should print 1582-10-2: " + day2.getYear() + "-" + day2.getMonth() + "-" + day2.getDate()) ;
+		System.out.println("Should print 3: " + day1.daysFrom(day2)) ;
+		System.out.println("Should print 0: " + day1.daysFrom(day1) + "\n") ;
+		
+		Day day3 = day2.addDays(81) ;
+		System.out.println("Should print 1583-1-1: " + day3.getYear() + "-" + day3.getMonth() + "-" + day3.getDate()) ;
+		System.out.println("Should print -81: " + day2.daysFrom(day3) + "\n") ;
+		
+		Day day4 = new Day(1 ,1, 1) ;
+		Day day5 = new Day(-1, 12, 30) ;
+		System.out.println("Should print 2: " + day4.daysFrom(day5)) ;
+		day5 = day4.addDays(-1) ;
+		System.out.println("Should print -1-12-31: " + day5.getYear() + "-" + day5.getMonth() + "-" + day5.getDate() + "\n") ;
+		
+		Day today = new Day(2019, 10, 6) ;
+		Day myBirthday = new Day(1998, 12, 21) ;
+		System.out.println("I have lived " + today.daysFrom(myBirthday) + " days.") ;
+		System.out.println("My 21st birthday is " + -today.daysFrom(new Day(2019, 12, 21)) + " days away!") ;
+	}
+}
